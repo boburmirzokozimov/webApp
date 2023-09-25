421daa67c566ed22c280dd4e9016a1aa4fd74a81
Boburmirzo Kozimov <boburmirzo@sabgames.org>
2023-09-25 10:58:45 +0500

    First Commits

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '6622358521:AAG7DVi0sMHsQwIEcP4WDq6-w6qK_C0v61A'
+const webAppUrl = 'https://ya.ru'
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+
+    bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if (text === '/start') {
+        await bot.sendMessage(chatId, 'Received your message!');
+
+        await bot.sendMessage(chatId, 'Click to start the app', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Start', web_app: {url: webAppUrl}}]
+                ]
+            }
+        })
+    }
+});
---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {RouterView} from 'vue-router'
+import {onMounted} from "vue";
+
+const tg = window.Telegram.WebApp
+
+onMounted(() => {
+  tg.ready()
+})
+
+const onClose = () => {
+  tg.close()
+}
+
+</script>
+
+<template>
+  <button @click="onClose">Close</button>
+  <RouterView/>
+</template>
+
+<style scoped>
+
+</style>
